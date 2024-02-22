0fca7fac9c08b47f64a8bd1a7b4511417d5d6b33
Asa <asabuckner19@gmail.com>
2024-02-22 05:11:54 -0600

    async get collections fin , unsplash endp fin

--- Documents/TSTP Tests/Modules/Mail/Global/Variables.py
+++ Documents/TSTP Tests/Modules/Mail/Global/Variables.py
@@ -1,3 +1,48 @@
+import requests
+import json
+import asyncio
+
+
+token = str
+secret = str
+app_id = str 
+
+with open('Global/credentials.json', "r") as f:
+    cred = json.loads(f.read())
+    token = cred["unsplash"]["access_key"]
+    secret = cred["unsplash"]["secret_key"]
+    app_id = cred["unsplash"]["application_id"]
+
+
 preferences = {"background_URL" : "", "theme" : "", }
 unsplash_link = "https://unsplash.com/"
-unsplash_url = "https://api.unsplash.com/"
+
+
+url = "https://api.unsplash.com/"
+
+get_collections= "{}collections/".format(url)
+#! Url / Collection ID
+get_collection_photos = "{}colections/{}/photos"
+#! URl / Photo ID
+get_photo = "{}photos/{}"
+
+
+post_personal_collection = "{}collections".format(url)
+#! URL / Personal Collection ID
+post_photo_Personal = "{}collections/{}/add"
+
+
+#! URL / Personal Collection ID
+delete_photo_Personal = "{}collections/{}/remove"
+
+
+
+def unsplash_get(endpoint):
+    response = requests.get(f"{endpoint}{token}")
+    if response.status_code == 200:
+        return response.json()
+    else:
+        response.raise_for_status() 
+    
+
+
--- Documents/TSTP Tests/Modules/Mail/Preferences/Background/CategorySelection.py
+++ Documents/TSTP Tests/Modules/Mail/Preferences/Background/CategorySelection.py
@@ -1,33 +1,23 @@
 import requests
-import json
 import sys
 import os
+import json
+import asyncio
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 mail_parent_dir = os.path.abspath(os.path.join(current_dir, '..', '..'))
 sys.path.append(mail_parent_dir)
 from Global import Variables
 
-
-token = str
-secret = str
-app_id = str 
-
 categories = []
 
-with open(os.path.abspath(os.path.join(mail_parent_dir, 'Global', 'credentials.json')), "r") as f:
-    cred = json.loads(f.read())
-    token = cred["unsplash"]["access_key"]
-    secret = cred["unsplash"]["secret_key"]
-    app_id = cred["unsplash"]["application_id"]
-
-# Define function to fetch data
-def get_categories():
+# Define asynchronous function to fetch data
+async def get_categories():
     try:
         # Make API request
-        res = requests.get("{}collections/?client_id={}".format(Variables.unsplash_url,token)).json()
-
-        for cat in res:
+        data =  Variables.unsplash_get(Variables.get_collections)
+        
+        for cat in data:
             source = cat.get("tags", [{}])[1].get("source", {})  # Get the source dictionary or an empty dictionary if tags or source are not found
             photographer_name = source.get("cover_photo", {}).get("user", {}).get("name", "Unknown")  # Get the photographer's name or "Unknown" if not found
 
@@ -49,5 +39,6 @@ def get_categories():
         print("Key not found in JSON:", e)
     except Exception as e:
         print("An unexpected error occurred:", e)
-        
-get_categories()
+
+# Run the asynchronous function
+asyncio.run(get_categories())
